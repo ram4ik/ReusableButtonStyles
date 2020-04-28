2516e6d9c1d95c81db1395b979086978b95dc415
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-28 22:41:01 +0300

    init

--- ReusableButtonStyles/ContentView.swift
+++ ReusableButtonStyles/ContentView.swift
@@ -8,9 +8,54 @@
 
 import SwiftUI
 
+struct NeumorphicButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.white.opacity(0.3), lineWidth: 2)
+                            .shadow(color: Color.black.opacity(0.3), radius: 2, x: 3, y: 3)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(color: Color.black.opacity(0.3), radius: 2, x: -2, y: -2)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                    } else {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(red: 225/255, green: 235/255, blue: 245/255))
+                            .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
+                            .shadow(color: Color.white.opacity(0.7), radius: 2, x: -2, y: -2)
+                    }
+                }
+            )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 225/255, green: 235/255, blue: 245/255)
+                .edgesIgnoringSafeArea(.all)
+            
+            HStack {
+                Button(action: {
+                    print("Pressed")
+                }) {
+                    Image(systemName: "wifi")
+                        .padding()
+                        .foregroundColor(Color.black.opacity(0.6))
+                }.buttonStyle(NeumorphicButtonStyle())
+                
+                Button(action: {
+                    print("Pressed")
+                }) {
+                    Image(systemName: "heart")
+                        .padding()
+                        .foregroundColor(Color.black.opacity(0.6))
+                }.buttonStyle(NeumorphicButtonStyle())
+            }
+        }
     }
 }
 
